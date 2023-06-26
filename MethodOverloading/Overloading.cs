0372be12d7666e986c6144c8a5b10c6b033e946d
SnehalKawade <snehkawade@gmail.com>
2023-06-26 14:44:15 +0530

    Add project files.

---
+++ MethodOverloading/Overloading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Overloading
+    {
+        public int Calculation(int a, int b)
+        {
+            return a - b;
+        }
+        public double Calculation(double x, double y)
+        {
+            return x * y;
+        }
+        public double Calculation(int p, double q)
+        {
+            return p / q;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Overloading o1 = new Overloading();
+           int result1 = o1.Calculation(30, 20);
+           double result2 = o1.Calculation(20.0,24.4);
+           double result3 = o1.Calculation(30,5);
+
+           Console.WriteLine(result1);
+           Console.WriteLine(result2);
+           Console.WriteLine(result3);
+        }
+    }
+}
